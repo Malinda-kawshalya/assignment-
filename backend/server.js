539992fa55f8098ce8ba59acf7ba3c5d18529907
Malinda Kawshalya <malindakawshalya@gmail.com>
2025-07-28 18:56:55 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -19,6 +19,7 @@ const corsOptions = {
     'http://localhost:3000',
     'http://localhost:5173', 
     'http://localhost:5174',
+    'https://assignment-ea7nhnpcd-malindakawshalya-8585s-projects.vercel.app',
     process.env.FRONTEND_URL || 'http://localhost:5173'
   ],
   credentials: true,
